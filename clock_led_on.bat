5e6057b5a7217b8aec1a071f99a9c3f77e619648
qihui-chen <2191424401@qq.com>
2023-09-18 15:07:03 +0800

    clock_led_on优化

--- clock_led_on.bat
+++ clock_led_on.bat
@@ -12,11 +12,7 @@ for /f  "skip=1 tokens=1 delims=	" %%i in (devices.txt) do (
 echo [LV]device ID is:%%i
 echo [LV]turn on clock leds...
 adb -s %%i shell "echo 160 | tee /sys/class/leds/cucumber\:white\:*/brightness"
-echo --------------------------------------------------------
-pause
-
-
-
+echo ---------------------------------------
 )
 rem del devices.txt
 echo --------------------------------------------------------
