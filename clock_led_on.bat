e0d1340bbd70cc83e6c974042c423a5d2babb9ed
unknown <346305@ggec.gd>
2023-09-18 09:29:00 +0800

    第二次修改

---
+++ clock_led_on.bat
@@ -0,0 +1,19 @@
+@echo off
+ 
+echo --------------------------------------------------------
+echo Get devices
+adb devices > devices.txt
+type devices.txt
+ 
+echo --------------------------------------------------------
+echo [LV]start turn on all clock leds
+echo -------------------------------------------------------- 
+for /f  "skip=1 tokens=1 delims=	" %%i in (devices.txt) do (
+echo [LV]device ID is:%%i
+echo [LV]turn on clock leds...
+adb -s %%i shell "echo 160 | tee /sys/class/leds/cucumber\:white\:*/brightness"
+echo --------------------------------------------------------
+)
+rem del devices.txt
+echo --------------------------------------------------------
+pause
